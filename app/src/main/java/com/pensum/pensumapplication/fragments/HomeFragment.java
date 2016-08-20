fbc77320c4361224aff75bdc72671a6020a0eb4e
violetaria <terri.chu@outlook.com>
2016-08-20 14:56:53 -0400

    load view pager in home fragment

--- app/src/main/java/com/pensum/pensumapplication/fragments/HomeFragment.java
+++ app/src/main/java/com/pensum/pensumapplication/fragments/HomeFragment.java
@@ -1,65 +1,32 @@
 package com.pensum.pensumapplication.fragments;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import com.parse.ParseFile;
-import com.parse.ParseUser;
 import com.pensum.pensumapplication.R;
+import com.pensum.pensumapplication.adapters.DashboardPagerAdapter;
 
 /**
  * Created by eddietseng on 8/19/16.
  */
 public class HomeFragment extends Fragment {
-    TextView tvName;
-    TextView tvEmail;
-    ImageView ivProfilePic;
-//    @BindView(R.id.viewpager) ViewPager viewPager;
-//    @BindView(R.id.sliding_tabs) TabLayout tabLayout;
-//    public static String POSITION = "POSITION";
+    public static String POSITION = "POSITION";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view =  inflater.inflate(R.layout.fragment_test, container, false);
 
-        tvName = (TextView) view.findViewById(R.id.tvName);
-        tvEmail = (TextView) view.findViewById(R.id.tvEmail);
-        ivProfilePic = (ImageView) view.findViewById(R.id.ivProfilePic);
+        View view =  inflater.inflate(R.layout.fragment_dashboard, container, false);
+        ViewPager viewPager = (ViewPager) view.findViewById(R.id.viewpager);
+        TabLayout tabLayout = (TabLayout) view.findViewById(R.id.sliding_tabs);
+        viewPager.setAdapter(new DashboardPagerAdapter(getChildFragmentManager(), getActivity()));
 
-        ParseUser user = ParseUser.getCurrentUser();
-        if (user != null) {
-            ivProfilePic.setImageResource(0);
-            tvName.setText(user.getUsername());
-
-            String fbName = (String) user.get("fbName");
-            tvEmail.setText(fbName);
-
-            try {
-                ParseFile parseFile = user.getParseFile("profileThumb");
-                byte[] data = parseFile.getData();
-                Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
-                ivProfilePic.setImageBitmap(bitmap);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-//
-//        getActivity().setContentView(R.layout.fragment_dashboard);
-//        View view = getView();
-//        ButterKnife.bind(view);
-//
-//        viewPager.setAdapter(new DashboardPagerAdapter(getChildFragmentManager(), getActivity()));
-//
-//        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);
 
         return view;
     }
