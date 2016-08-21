fadb056103a712c45e9e3c56b746272fd28424da
dhalima3 <dhalima@gatech.edu>
2016-08-21 18:22:21 -0400

    search for gridview

--- app/src/main/java/com/pensum/pensumapplication/activities/HomeActivity.java
+++ app/src/main/java/com/pensum/pensumapplication/activities/HomeActivity.java
@@ -77,9 +77,7 @@ public class HomeActivity extends AppCompatActivity implements AddTaskFragment.O
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
 
     public void selectDrawerItem(MenuItem menuItem) {
